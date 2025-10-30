6c1fddc6dad6a74bba73ad967cae9b084315abe8
Shahid Rafique <shahid4it@gmail.com>
2025-10-31 00:14:28 +0500

    remove icon from service

--- frontend/src/app/components/services/services.tsx
+++ frontend/src/app/components/services/services.tsx
@@ -33,38 +33,7 @@ export function Services({ title, content, services = [] }) {
                   </figure>
                   <div className="service-card__content">
                     <Link href={`/services/${slug}`}>
-                      <h3>
-                        {title}{" "}
-                        <svg
-                          width="32"
-                          height="33"
-                          viewBox="0 0 32 33"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M28 17.6768V25.6768C28 26.3841 27.719 27.0624 27.219 27.5625C26.7189 28.0626 26.0406 28.3435 25.3333 28.3435H6.66667C5.95942 28.3435 5.28115 28.0626 4.78105 27.5625C4.28095 27.0624 4 26.3841 4 25.6768V7.01017C4 6.30293 4.28095 5.62465 4.78105 5.12455C5.28115 4.62446 5.95942 4.34351 6.66667 4.34351H14.6667"
-                            stroke="white"
-                            strokeWidth="3.64209"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                          <path
-                            d="M28 4.34351L16 16.3435"
-                            stroke="white"
-                            strokeWidth="3.64209"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                          <path
-                            d="M20 4.34351H28V12.3435"
-                            stroke="white"
-                            strokeWidth="3.64209"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </h3>
+                      <h3>{title}</h3>
                     </Link>
                     <p className="body-mid">{content}</p>
                   </div>
